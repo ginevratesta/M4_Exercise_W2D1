100a58462c809e00758879fad356a99e4bcd8dba
Ginevra Testa <ginevra99testaa@gmail.com>
2024-01-09 16:41:38 +0100

    html display

--- script.js
+++ script.js
@@ -7,29 +7,38 @@
 // .then(data => console.log(data))
 // .catch(error => console.error(error))
 
-
 // self invoked function - IIFE
-(async() => {
-    const response = await fetch('https://api.pexels.com/v1/search?query=gatto',{
-        headers: {
-            'authorization': 'fleEjrFkHZgMxtSgwXjLcn8VDvBSo4xHjoQbCwIF42goR6dhhsdCATgm'
-        }
-    })
-   const data = await response.json()
-})() 
+(async () => {
+  const response = await fetch("https://api.pexels.com/v1/search?query=gatto", {
+    headers: {
+      authorization: "fleEjrFkHZgMxtSgwXjLcn8VDvBSo4xHjoQbCwIF42goR6dhhsdCATgm",
+    },
+  });
+  const data = await response.json();
+})();
 
-fetch('https://api.pexels.com/v1/search?query=gatto',{
-headers: {
-    'authorization': 'fleEjrFkHZgMxtSgwXjLcn8VDvBSo4xHjoQbCwIF42goR6dhhsdCATgm'
-}
+fetch("https://api.pexels.com/v1/search?query=gatto", {
+  headers: {
+    authorization: "fleEjrFkHZgMxtSgwXjLcn8VDvBSo4xHjoQbCwIF42goR6dhhsdCATgm",
+  },
 })
-.then(res => res.json())
-.then(data => {
-    const items = data.photos;
-    for(item of items){
-        const pics = item.src.medium; 
-        const detail = item.alt;
+  .then((res) => res.json())
+  .then((data) => {
+    const arrayOfCats = data.photos;
+    const cards = document.querySelector(".row");
+    for (picsOfCats of arrayOfCats) {
+      const pics = picsOfCats.src.medium;
+      const detail = picsOfCats.alt;
+      console.log(pics, detail);
+      cards.innerHTML += `
+        <div class="col-lg-3"
+         <div class="card">
+          <img src="${pics}" class="card-img-top" alt="Cat pic">
+           <div class="card-body">
+               <p class="card-text">${detail}</p>
+          </div>
+        </div>
+       </div>`;
     }
-    console.log(pics,detail);
-})
-.catch(error => console.error(error))
+  })
+  .catch((error) => console.error(error));
