6e10d33f35495b319f35a901d3c923c2905eacd9
Ginevra Testa <ginevra99testaa@gmail.com>
2024-01-09 16:44:47 +0100

    adjustments

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-// fetch('https://api.pexels.com/v1/search?query=gatto',{
-//     headers: {
-//         'authorization': 'fleEjrFkHZgMxtSgwXjLcn8VDvBSo4xHjoQbCwIF42goR6dhhsdCATgm'
-//     }
-// })
-// .then(res => res.json())
-// .then(data => console.log(data))
-// .catch(error => console.error(error))
-
 // self invoked function - IIFE
-(async () => {
-  const response = await fetch("https://api.pexels.com/v1/search?query=gatto", {
-    headers: {
-      authorization: "fleEjrFkHZgMxtSgwXjLcn8VDvBSo4xHjoQbCwIF42goR6dhhsdCATgm",
-    },
-  });
-  const data = await response.json();
-})();
+
+// (async () => {
+//   const response = await fetch("https://api.pexels.com/v1/search?query=gatto", {
+//     headers: {
+//       authorization: "fleEjrFkHZgMxtSgwXjLcn8VDvBSo4xHjoQbCwIF42goR6dhhsdCATgm",
+//     },
+//   });
+//   const data = await response.json();
+// })();
+// fetch("https://api.pexels.com/v1/search?query=gatto", {
+//   headers: {
+//     authorization: "fleEjrFkHZgMxtSgwXjLcn8VDvBSo4xHjoQbCwIF42goR6dhhsdCATgm",
+//   },
+// })
+//   .then((res) => res.json())
+//   .then((data) => console.log(data))
+//   .catch((error) => console.error(error));
 
 fetch("https://api.pexels.com/v1/search?query=gatto", {
   headers: {
