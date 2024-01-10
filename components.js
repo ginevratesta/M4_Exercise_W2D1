c261c1283ce13b4ce58483389d652b9ce8ffe120
Ginevra Testa <ginevra99testaa@gmail.com>
2024-01-10 18:55:08 +0100

    css application

--- components.js
+++ components.js
@@ -1,13 +1,11 @@
-
-
-export const displayCards = (pics, detail) => (
-    `
+export const displayCards = (pics, detail) => 
+  `
     <div class="col-lg-3"
      <div class="card">
-      <img src="${pics}" class="card-img-top" alt="Cat pic">
+      <img src="${pics}" class="card-img-top w-100 h-100" alt="Cat pic">
        <div class="card-body">
            <p class="card-text">${detail}</p>
       </div>
     </div>
-   </div>`
-);
+   </div>`;
+
--- script.js
+++ script.js
@@ -1,27 +1,8 @@
 import {displayCards} from './components.js';
 
-// self invoked function - IIFE
-
-// (async () => {
-//   const response = await fetch("https://api.pexels.com/v1/search?query=gatto", {
-//     headers: {
-//       authorization: "fleEjrFkHZgMxtSgwXjLcn8VDvBSo4xHjoQbCwIF42goR6dhhsdCATgm",
-//     },
-//   });
-//   const data = await response.json();
-// })();
-// fetch("https://api.pexels.com/v1/search?query=gatto", {
-//   headers: {
-//     authorization: "fleEjrFkHZgMxtSgwXjLcn8VDvBSo4xHjoQbCwIF42goR6dhhsdCATgm",
-//   },
-// })
-//   .then((res) => res.json())
-//   .then((data) => console.log(data))
-//   .catch((error) => console.error(error));
-
 const cards = document.querySelector(".row");
 
-fetch("https://api.pexels.com/v1/search?query=clowns", {
+fetch("https://api.pexels.com/v1/search?query=clown", {
   headers: {
     authorization: "fleEjrFkHZgMxtSgwXjLcn8VDvBSo4xHjoQbCwIF42goR6dhhsdCATgm",
   },
@@ -33,7 +14,6 @@ fetch("https://api.pexels.com/v1/search?query=clowns", {
     // renderizza qualcosa
     cards.innerHTML +=  displayCards(item.src.medium, item.alt)
     })
-
   })
   .catch((error) => console.error(error));
 
@@ -41,18 +21,4 @@ fetch("https://api.pexels.com/v1/search?query=clowns", {
 
 
 
-//     for (itemFromArray of items) {
-//       const pics = itemFromArray.src.medium;
-//       const detail = itemFromArray.alt;
-//       console.log(pics, detail);
-//       cards.innerHTML += `
-//         <div class="col-lg-3"
-//          <div class="card">
-//           <img src="${pics}" class="card-img-top" alt="Cat pic">
-//            <div class="card-body">
-//                <p class="card-text">${detail}</p>
-//           </div>
-//         </div>
-//        </div>`;
-//     }
-//   })
+
